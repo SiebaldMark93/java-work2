f72e506d1b6489dc25373c3125051f1e79ba5df8
Mark Siebald <siebald.mark93@gmail.com>
2022-12-06 09:50:21 -0600

    practicing variables

---
+++ src/Variables.java
@@ -0,0 +1,37 @@
+public class Variables {
+    public static void main(String[] args) {
+        // x = 10
+        // x + 5 = 15
+
+        // a = 7
+        // b = 9
+        // b - a = 12
+
+        // violin = 3
+        // piano = 5
+        // violin * piano = 15
+
+        // Assign the type of value
+        // In java values are stored in memory
+        // = is the assignment operator
+        // int = whole numbers
+        int x = 10;
+        System.out.println(x);
+
+        int drums = 25;
+        System.out.println(drums);
+
+        //We can add, subtract, multiply or divide within the println
+        System.out.println(drums * 2);
+        int trumpet = 15 / 5;
+        System.out.println(trumpet + 10);
+
+        //can use () for order of operations
+        int flute = 5 * 20 / 2;
+        System.out.println(flute);
+
+        int flute2 = 5 * (20 / 4);
+        System.out.println(flute2);
+        System.out.println(flute2 - 10);
+    }
+}
