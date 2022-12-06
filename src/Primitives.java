dcdfd1cee9fae6d77e57c35ae88d80033cc79989
Mark Siebald <siebald.mark93@gmail.com>
2022-12-06 10:08:09 -0600

    feat. primitive data types

---
+++ src/Primitives.java
@@ -0,0 +1,20 @@
+public class Primitives {
+    public static void main(String[] args) {
+        //Primitive Data Types
+        // Data Type    |   Value Range
+        // byte         |   -128 to 127
+        // short        |   -32768 to 32767 ( 2 bytes of memory)
+        // int          |   -2147483648 to 2147483648 (4 bytes of memory)
+        // long         |   -9223372036854775808 to 9223372036854775807
+        // float        |   varies (decimals)
+        // double       |   varies (decimals) default data type for decimals
+        // boolean      |   true/false
+        // char         |   0 to 65535
+
+        int guitar = 20;
+        double x = 2.5;
+        double y = guitar - x;
+        System.out.println(guitar - x);
+        System.out.println(y);
+    }
+}
