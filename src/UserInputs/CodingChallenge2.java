f137889e2d8f821a2c2d56516342cda1153cac03
Mark Siebald <siebald.mark93@gmail.com>
2022-12-11 19:12:45 -0600

    feat. coding challenge with name length and bill calculator

--- src/UserInputs/CodingChallenge2.java
+++ src/UserInputs/CodingChallenge2.java
@@ -12,13 +12,30 @@ public class CodingChallenge2 {
         int age = scanner.nextInt();
 
         System.out.println("Your name is " + firstName + " and you are " + age + " years old.");
+        //Print out the number of characters in your name
+        System.out.println("Your name is " + firstName.length() + " characters long.");
 
-        System.out.println("Amount of money in your pocket: f");
+        //Print "You've lived x years.  In another x years you'll be 2x years old." Where x is age
+        System.out.println("You lived " + age + " years." + " In another " + age + " years you'll be " + (age * 2) + " years old." );
+
+        System.out.println("Amount of money in your pocket: ");
         double money = scanner.nextDouble();
 
-        System.out.println(money);
+        System.out.println();
 
         double moneyNeeded = Math.ceil(money) - money;
-        System.out.println("You have ");
+        System.out.format("You have %.2f on you and you need %.2f.", money, moneyNeeded);
+
+        //Calculate the bill
+        System.out.println("\nRestaurant Bill Calculator\n");
+        System.out.println("Enter bill total: ");
+        double bill = scanner.nextDouble();
+
+        System.out.println("Enter number of people: ");
+        int people = scanner.nextInt();
+
+        double eachPay = bill / people;
+
+        System.out.format("A bill of %.2f split between " + people + " people comes to approximately %.2f each", bill, eachPay);
     }
 }
