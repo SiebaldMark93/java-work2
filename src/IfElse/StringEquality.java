f39c31db7d270ceabdf1ef910e1ce4932ae46f1c
Mark Siebald <siebald.mark93@gmail.com>
2022-12-14 09:46:41 -0600

    feat. nesting an if statement inside of an if statement

--- src/IfElse/StringEquality.java
+++ src/IfElse/StringEquality.java
@@ -18,15 +18,34 @@ public class StringEquality {
         System.out.print("Age: ");
         int age = scanner.nextInt();
 
-        System.out.println("Do you have a VIP pass?  yes/no: ");
-        String vipPassReply = scanner.next();
+//        System.out.println("Do you have a VIP pass?  yes/no: ");
+//        String vipPassReply = scanner.next();
 
         //We can add a third condition like free if you are 50 years old or older. Using ||
-        if (age >= 18 && vipPassReply.equals("yes") || age >= 50 ) {
-            System.out.println("Please enjoy your time.");
+//        if (age >= 18 && vipPassReply.equals("yes") || age >= 50 ) {
+//            System.out.println("Please enjoy your time.");
+//        }
+//        else {
+//            System.out.println("Sorry, you can't come in.");
+//        }
+
+        //Nesting an if-statement inside an if.
+
+        if (age >= 18) {
+            //move the code in line 21-22 inside this if in order for the under 18 response to work better
+            System.out.println("Do you have a VIP Pass? yes/no: ");
+            String vipPassReply = scanner.next();
+
+            if (vipPassReply.equals("yes") || age >= 50) {
+                System.out.println("Please enjoy your time");
+            }
+            else {
+                System.out.println("Sorry you must be over 50 or have a VIP Pass.");
+            }
         }
+        //add an outside else statement for if they are younger than 18
         else {
-            System.out.println("Sorry, you can't come in.");
+            System.out.println("Sorry you are too young.");
         }
     }
 }
