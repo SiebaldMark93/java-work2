b6206b310ee9ad0bb182a21db5c98ee903f0dea3
Mark Siebald <siebald.mark93@gmail.com>
2023-02-17 10:32:33 -0600

    feat. do while loop

--- src/Loops/doWhileLoop.java
+++ src/Loops/doWhileLoop.java
@@ -46,10 +46,21 @@ public class doWhileLoop {
             /*Store the user's answer of (y/n) in a String variable
               Declare the String variable outside the do while to be able to access it both inside and outside the loop.
              */
+
              again = scanner.next();
              //equals will grab the user's response of "y", otherwise "n" or any other value will be deemed false and stop the loop.
             //Do while loop will keep running until the user inputs "n" or false.
         } while(again.equals("y"));
+
+        //To reverse the boolean statement
+//        do {
+//            int num2 = ThreadLocalRandom.current().nextInt(1, 7);
+//            System.out.println("You rolled " + num2 + "!");
+//            //Ask to stop rolling
+//            System.out.print("Stop rolling? y/n: ");
+//            again = scanner.next();
+//            //using the reverse operator "!" reverses the input of y from previous do while loop and stops the loop if "y" is inputted.
+//        } while(!again.equals("y"));
 
 
     }
